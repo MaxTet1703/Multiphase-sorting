4073acaf35adbd26577c7222d38f5287a7d0c853
MaxTet1703 <maksimtetuskin1267@mail.ru>
2023-11-24 04:55:44 +0700

    Начал писать сортировку для csv файлов

--- my_sort.py
+++ my_sort.py
@@ -11,10 +11,12 @@ error = CURRENT_PATH / "sdfsdfsf.txt"
 def my_sort(src: PathType, output: Optional[PathType] = None, reverse: bool = False,
             key: Optional[Callable] = None) -> None:
     LIMIT_ELEMENT = 40
+
     if not src.exists():
         raise FileExistsError("File is not exist in the directory")
 
     files = list(CURRENT_PATH.glob(f'*{src.suffix}'))
+    seq_count = len(files)
     for name in files:
         if name.stem == src.stem:
             files.remove(name)
@@ -34,8 +36,8 @@ def my_sort(src: PathType, output: Optional[PathType] = None, reverse: bool = Fa
 
     files.append(output)
 
-    distr = [0 if i == len(files) - 1 else 1 for i in range(len(files))]
-    empty_series = [0 if i == len(files) - 1 else 1 for i in range(len(files))]
+    distr = [0 if i == seq_count - 1 else 1 for i in range(seq_count)]
+    empty_series = [0 if i == seq_count - 1 else 1 for i in range(seq_count)]
 
     if src.suffix == ".csv":
         current_line = 1
@@ -55,7 +57,7 @@ def my_sort(src: PathType, output: Optional[PathType] = None, reverse: bool = Fa
             if empty_series[cfile] == 0:
                 loops += 1
                 distr0 = distr[0]
-                for i in range(len(files) - 1):
+                for i in range(seq_count - 1):
                     empty_series[i] = distr0 + distr[i + 1] - distr[i]
                     distr[i] = distr0 + distr[i + 1]
             cfile = 0
@@ -66,10 +68,45 @@ def my_sort(src: PathType, output: Optional[PathType] = None, reverse: bool = Fa
         current_line += 1
 
     if set(empty_series) != {0}:
-        distribution(files, empty_series, distr)
+        distribution(files, empty_series)
+        loops += 1
+
+    length_of_series = [0 if i == seq_count else 1 for i in range(seq_count)]
+    number_of_series = distr
+    list_for_merge = []
+    del distr
 
 
-def distribution(files: list, empty_series: list, distr: list):
+    for cfile in range(seq_count-1):
+        series = files[cfile].open(mode="r").readlines()[:length_of_series[cfile]+1]
+        series_csv = csv.DictReader(series, delimiter=";")
+        for ser in series_csv:
+            list_for_merge.append(ser)
+        new_data = files[cfile].open(mode="r").readlines()
+        new_data[1:length_of_series[cfile]+1] = ["" for _ in range(length_of_series[cfile])]
+        files[cfile].open(mode="w").writelines(new_data)
+    print(list_for_merge)
+    real_series = [element for element in list_for_merge if key(element) not in ("", "none")]
+    empty_series = [element for element in list_for_merge if key(element) in ("", "none")]
+    sort_series = []
+    while real_series:
+        selected = min(real_series, key=key)
+        sort_series.append(selected)
+        index_selected = real_series.index(selected)
+        real_series.pop(index_selected)
+    if empty_series:
+        sort_series.extend(empty_series)
+    write_csv = csv.DictWriter(files[-1], delimiter=";", fieldnames=list(selected.keys()))
+    for row in sort_series:
+        write_csv.writerow(row)
+
+
+
+
+
+
+
+def distribution(files: list, empty_series: list):
     resum = sum(empty_series)
     cfile = 0
     while resum > 0:
@@ -78,7 +115,9 @@ def distribution(files: list, empty_series: list, distr: list):
         else:
             cfile = 0
 
-        files[cfile].open(mode="a").write("none\n")
+        files[cfile].open(mode="+a").write("none;\n")
         resum -= 1
-        empty_series[cfile] -=1
+        empty_series[cfile] -= 1
 
+
+my_sort(source, key = lambda x: x['Возраст'])
